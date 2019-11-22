fbbb1f475282273ef6fb20696e644b19d9b762db
cofoeducistudent <cofoedu91@gmail.com>
2019-11-22 15:20:16 +0000

    re-arranged navigation bar, split out Start game for better user experience

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,5 @@
-
 // Define Variables
 var tick = 0;
-
-
-
-
-
 // Clear Flashing
 function clearFlasher(){
 $('flash1').removeClass('makeRed').removeClass('makeBlue').removeClass('makeYellow').removeClass('makeGreen');
@@ -15,57 +9,45 @@ $('flash3').removeClass('makeRed').removeClass('makeBlue').removeClass('makeYell
 
 
 
+
+
+
 // Flasher
-$(document).ready(function(){
-
     function flasher(){
-
         alert('pop');
-        
-
         tick ++;
-        if(tick > 3){
+        
+        if(tick > 3)
+        {
             tick = 0;
             return;
         }
-        
-        
+
+
         switch(tick){
-        
         case 0:
         clearFlasher();
         $('flash1').addClass('makeRed');
         $('flash2').addClass('makeBlue');
         $('flash3').addClass('makeYellow');
         return;
-        
         case 1:
         clearFlasher();
         $('flash1').addClass('makeBlue');
         $('flash2').addClass('makeYellow');
         $('flash3').addClass('makeGreen');
         return;
-        
         case 2:
         clearFlasher();
         $('flash1').addClass('makeYellow');
         $('flash2').addClass('makeGreen');
         $('flash3').addClass('makeRed');
         return;
-        
         case 3:
         clearFlasher();
         $('flash1').addClass('makeBlue');
         $('flash2').addClass('makeYellow');
         $('flash3').addClass('makeGreen');
         return;
-  
-        
         }
-        
         }
-
-
-
-
-})
