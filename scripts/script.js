4da213a9cdd722a1063339a3b1d7f96fb9802b83
cofoeducistudent <cofoedu91@gmail.com>
2019-11-22 09:12:43 +0000

    adjusted homepage image size

---
+++ scripts/script.js
@@ -0,0 +1,71 @@
+
+// Define Variables
+var tick = 0;
+
+
+
+
+
+// Clear Flashing
+function clearFlasher(){
+$('flash1').removeClass('makeRed').removeClass('makeBlue').removeClass('makeYellow').removeClass('makeGreen');
+$('flash2').removeClass('makeRed').removeClass('makeBlue').removeClass('makeYellow').removeClass('makeGreen');
+$('flash3').removeClass('makeRed').removeClass('makeBlue').removeClass('makeYellow').removeClass('makeGreen');
+}
+
+
+
+// Flasher
+$(document).ready(function(){
+
+    function flasher(){
+
+        alert('pop');
+        
+
+        tick ++;
+        if(tick > 3){
+            tick = 0;
+            return;
+        }
+        
+        
+        switch(tick){
+        
+        case 0:
+        clearFlasher();
+        $('flash1').addClass('makeRed');
+        $('flash2').addClass('makeBlue');
+        $('flash3').addClass('makeYellow');
+        return;
+        
+        case 1:
+        clearFlasher();
+        $('flash1').addClass('makeBlue');
+        $('flash2').addClass('makeYellow');
+        $('flash3').addClass('makeGreen');
+        return;
+        
+        case 2:
+        clearFlasher();
+        $('flash1').addClass('makeYellow');
+        $('flash2').addClass('makeGreen');
+        $('flash3').addClass('makeRed');
+        return;
+        
+        case 3:
+        clearFlasher();
+        $('flash1').addClass('makeBlue');
+        $('flash2').addClass('makeYellow');
+        $('flash3').addClass('makeGreen');
+        return;
+  
+        
+        }
+        
+        }
+
+
+
+
+})
